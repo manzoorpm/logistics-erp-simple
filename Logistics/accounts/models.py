ed7522da496ff8a52a540906a50c7072761bb710
manzoorpm <manzoorpm@live.com>
2022-05-22 14:31:09 -0400

    Models bug fix

--- Logistics/accounts/models.py
+++ Logistics/accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Warehouse(models.Model):
+	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
@@ -14,6 +15,7 @@ class Warehouse(models.Model):
 
 
 class Tag(models.Model):
+	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
@@ -21,10 +23,11 @@ class Tag(models.Model):
 
 class Product(models.Model):
 	CATEGORY = (
-			('Liquid', 'Liquid'),
+			('Liquid', 'Liquid'),   
 			('Non Liquid', 'Non Liquid'),
 			) 
 
+	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
@@ -43,6 +46,7 @@ class Order(models.Model):
 			('Pending', 'Pending'),
 			)
 
+	id = models.AutoField(primary_key=True)
 	warehouse = models.ForeignKey(Warehouse, null=True, on_delete= models.SET_NULL)
 	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
